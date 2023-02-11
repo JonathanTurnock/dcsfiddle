e59b2b0d395e7b1b9b7d806e041f2e94d171e0f1
fyyyyy <frankyyyy@live.com>
2023-02-11 14:01:14 +0100

    Add option to open large files in new tab, with syntax coloring

--- src/components/Response.jsx
+++ src/components/Response.jsx
@@ -5,8 +5,39 @@ import { isObject } from "lodash";
 import { Prism } from "@mantine/prism";
 import fileDownload from "js-file-download";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faDownload, faWindowMaximize } from "@fortawesome/free-solid-svg-icons";
 const bytes = require("bytes");
+
+function syntaxHighlight(json) {
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+      var cls = 'number';
+      if (/^"/.test(match)) {
+          if (/:$/.test(match)) {
+              cls = 'key';
+          } else {
+              cls = 'string';
+          }
+      } else if (/true|false/.test(match)) {
+          cls = 'boolean';
+      } else if (/null/.test(match)) {
+          cls = 'null';
+      }
+      return '<span class="' + cls + '">' + match + '</span>';
+  });
+}
+
+const colorStyle = `<style>
+body {
+  background: #111;
+  color: orange;
+}
+pre {outline: 1px solid #ccc; padding: 5px; margin: 5px; }
+.string { color: springgreen; }
+.number { color: cyan; }
+.boolean { color: blue; }
+.null { color: magenta; }
+.key { color: white; }</style>`;
 
 export const Response = ({ date, response }) => {
   const [collapsed, toggleCollapse] = useToggle(false);
@@ -17,7 +48,7 @@ export const Response = ({ date, response }) => {
 
   const responseBytes = displayRes.length;
 
-  const isTooLarge = responseBytes > bytes("1mb");
+  const isTooLarge = responseBytes > bytes("100kb");
 
   const tooLargeMessage = `The response is ${bytes(
     responseBytes
@@ -30,6 +61,17 @@ export const Response = ({ date, response }) => {
         <Text size={"sm"}>{date}</Text>
         <ActionIcon onClick={() => fileDownload(displayRes, `${date}.json`)}>
           <FontAwesomeIcon icon={faDownload} />
+        </ActionIcon>
+        <ActionIcon onClick={() => {
+          const pretty = JSON.stringify(response, undefined, 2)
+          const colored = syntaxHighlight(pretty)
+          const newWindow = window.open("json","_blank");
+          newWindow.document.write('<html><body><pre>' + colored + '</pre></body></html>' )
+          newWindow.document.write(colorStyle)
+          newWindow.focus();
+        }
+        }>
+          <FontAwesomeIcon icon={faWindowMaximize} />
         </ActionIcon>
       </Group>
       <Collapse in={!collapsed}>
