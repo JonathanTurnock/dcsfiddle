ab514aa2636c4bcc4f26e654d8cbb447bde8bdcf
🐸 <dice-a6db@outlook.com>
2023-12-17 22:27:04 +0000

    feat: improve explorer panel

--- src/components/ExploreModal.jsx
+++ src/components/ExploreModal.jsx
@@ -1,7 +1,12 @@
-import { List, Modal, Stack, Text, Title } from "@mantine/core";
+import { List, Modal, Stack, Text, TextInput, Title } from "@mantine/core";
 import { ExploreNode } from "./ExploreNode";
+import { useState } from "react";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const ExploreModal = ({ onClose, open }) => {
+  const [filter, setFilter] = useState("");
+
   return (
     <Modal
       title={<Title variant="h2">Explorer</Title>}
@@ -15,8 +20,16 @@ export const ExploreModal = ({ onClose, open }) => {
           From here you can explore the DCS scripting environment, functions
           need to be called via a script.
         </Text>
+
+        <TextInput
+          icon={<FontAwesomeIcon icon={faSearch} />}
+          placeholder={"Search within results..."}
+          defaultValue={filter}
+          onBlur={(ev) => setFilter(ev.target.value)}
+        />
+
         <List>
-          <ExploreNode k={"_G"} v={"root"} scope={[]} />
+          <ExploreNode k={"_G"} v={"root"} scope={[]} filter={filter} />
         </List>
       </Stack>
     </Modal>
